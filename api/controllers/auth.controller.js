b8c1d91a38ff58f2ae075c2f44c006ed7ba09665
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-27 19:28:32 +0500

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const signUp = asyncHandler(async (req, res) => {
   try {
@@ -26,4 +27,32 @@ const signUp = asyncHandler(async (req, res) => {
   }
 });
 
-export { signUp };
+const signIn = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if ([email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      throw new ApiError(404, "User not found");
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      throw new ApiError(401, "Invalid credentials");
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...others } = validUser._doc;
+    return res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(new ApiResponse(200, others, "Signed in successfully"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+export { signUp, signIn };
