a06276361c332300527077325b4c548ee8a42b8d
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-27 15:11:44 +0500

    create the header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,63 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+function Header() {
+  const path = useLocation().pathname;
+  return (
+    <>
+      <Navbar className="border-b-2">
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Habibian
+          </span>
+          Blog
+        </Link>
+        {/* Navbar Search */}
+        <form>
+          <TextInput
+            type="text"
+            placeholder="Search"
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+          />
+        </form>
+        {/* Navbar search button for small screen */}
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+          <AiOutlineSearch />
+        </Button>
+        {/* Navbar right section: buttons and dark mode  */}
+        <div className="flex gap-2 md:order-2">
+          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          </Link>
+
+          {/* auto: show Menubar icon for small screen */}
+          <Navbar.Toggle />
+        </div>
+
+        {/* Navbar collapse: auto hide on medium screens (i) */}
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
