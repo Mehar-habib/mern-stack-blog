0475d9261821382ef845b1215a043948fc3c1881
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-30 23:22:37 +0500

    create get posts api route

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyUserToken } from "../middlewares/verifyUser.js";
-import { createAdminPost } from "../controllers/post.controller.js";
+import { createAdminPost, getPosts } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.post("/create", verifyUserToken, createAdminPost);
+router.get("/get-posts", getPosts);
 
 export default router;
